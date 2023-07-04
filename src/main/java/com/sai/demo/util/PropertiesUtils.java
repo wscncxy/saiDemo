ad7ac591e1c71eef604b3c1a80832693dd6547be
sai <wscncxy@gmail.com>
2023-07-04 20:55:39 +0800

    to 17

--- src/main/java/com/sai/demo/util/PropertiesUtils.java
+++ src/main/java/com/sai/demo/util/PropertiesUtils.java
@@ -4,13 +4,13 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.Properties;
 
-public class PropertiesUtils extends Properties{
+public class PropertiesUtils extends Properties {
     private final String classRootPath = PropertiesUtils.class.getResource("/").getPath();
 
     public PropertiesUtils() {
         try {
             // 使用ClassLoader加载properties配置文件生成对应的输入流
-            BufferedReader in = new BufferedReader(new FileReader(classRootPath+"/generatorConfig.properties"));
+            BufferedReader in = new BufferedReader(new FileReader(classRootPath + "/generatorConfig.properties"));
             // 使用properties对象加载输入流
             this.load(in);
         } catch (Exception e) {
